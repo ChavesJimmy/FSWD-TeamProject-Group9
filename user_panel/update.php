0e2235c226ce9aae10daf9c16df1873c0153056f
DejanBicok <bicokd@hotmail.com>
2022-12-06 17:03:46 +0100

    fileupload //

--- user_panel/update.php
+++ user_panel/update.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once '../components/db_connect.php';
-require_once '../components/file_upload.php';
+// require_once '../components/file_upload.php';
 
 
 // if (!isset($_SESSION['adm']) && !isset($_SESSION['user'])) {
