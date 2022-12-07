34bd3b5eacbdbda7bed954e9a87e997cbd605b84
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-12-07 16:08:06 +0100

    added review display and form

---
+++ products/reviews.php
@@ -0,0 +1,77 @@
+<?php
+//session_start();
+require_once '../components/db_connect.php' ;
+
+/* if (isset($_SESSION['user']) != "" ) {
+  header("Location: ../home.php");
+  exit;
+}
+
+if (! isset($_SESSION['adm']) && !isset($_SESSION['user' ])) {
+  header("Location: ../index.php");
+  exit;
+} */
+//$id=$_GET['id'];
+//mmmust change 3 by get id
+$sql = "SELECT * FROM products_reviews WHERE fk_product=3";
+$result = mysqli_query($connect, $sql);
+$sql2 = "SELECT * FROM products WHERE id=3";
+$result2 = mysqli_query($connect, $sql2);
+$row2=mysqli_fetch_array($result2, MYSQLI_ASSOC);
+$tbody = ''; 
+if (mysqli_num_rows($result)  > 0) {
+  while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+      $tbody .= "<div id='review'>
+      <h1>Review" .$row2['name']."</h1>
+      <h3>Rating ".$row['star']."</h3>
+      <p>".$row['message']."</p>
+      </div>" ;
+  };
+} else {
+  $tbody =  "<tr><td colspan='5'><center>No Data Available </center></td></tr>" ;
+}
+
+mysqli_close($connect);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta  charset="UTF-8">
+  <meta name="viewport"  content="width=device-width, initial-scale=1.0">
+  <title>Reviews <?= $row2['name']?></title>
+   <?php  require_once '../components/boot.php' ?>
+  <style>
+    #review{
+        border: solid 2px;
+        padding: 1rem;
+    }
+  </style>
+</head>
+
+<body>
+  <div class= "manageProduct w-75 mt-3">
+      <p class='h2'> Reviews </p>
+      
+              <?=$tbody;?>
+  </div>
+  <form action="actions/a_review.php" method="post">
+    <h1>Add review for <?= $row2['name']?></h1>
+    <label for="review">your review</label>
+    <textarea  class="form-select" name="message" id="" cols="30" rows="10"></textarea>
+    <label for="rating">Rating</label>
+    <select class="form-select" name="star" id="">
+        <option value="0">0</option>
+        <option value="1">⭐</option>
+        <option value="2">⭐⭐</option>
+        <option value='3'>⭐⭐⭐</option>
+        <option value="4">⭐⭐⭐⭐</option>
+        <option value="5">⭐⭐⭐⭐⭐</option>
+    </select>
+    <input type="hidden" name="product" value="<?=$row2['id']?>">
+    <input type="hidden" name="user" value="">
+    <button class='btn btn-success' type="submit">Send review</button>
+  </form>
+</body>
+</html> 
