22a515f47f4e53a21b3888af629c7b4b74ce928e
juliawegmayr <kontakt@juliawegmayr.at>
2022-12-07 11:49:00 +0100

    correct typo

--- products/details.php
+++ products/details.php
@@ -24,7 +24,7 @@ if ($_GET['id']) {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Details - Animal Adoption</title>
+    <title>Details - Products</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
