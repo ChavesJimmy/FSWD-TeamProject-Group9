5ddad55e2d821af7227d2d2bd898334a7b8e724f
juliawegmayr <kontakt@juliawegmayr.at>
2022-12-11 12:07:16 +0100

    Uncomment Check Connection

--- components/db_connect.php
+++ components/db_connect.php
@@ -8,11 +8,11 @@ $dbname = "fswd-teamproject_group9";
 // create connection
 $connect = new  mysqli($localhost, $username, $password, $dbname);
 
-// check connection
+/* check connection
 if ($connect->connect_error) {
   die("Connection failed: " . $connect->connect_error);
 } else {
   echo "Successfully Connected";
 }
-
+*/
 ?>
---
+++ contact/contact.php
@@ -0,0 +1,26 @@
+   <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title></title>
+   </head>
+   <body>
+
+        <p>Contact Us</p>
+       <form action="form.php" method ="POST">
+           Name: <input type="text"  name="name" />
+           Email: <input type="email" name="email" />
+           Subject: <input type ="text" name="subject" />
+           Message: <input type="textfield" name="message" />
+           <input  type="submit" name="submit" value="Submit" />
+       </form>
+      
+   </body>
+</html>
+
+
+
+
+
+
+
+
