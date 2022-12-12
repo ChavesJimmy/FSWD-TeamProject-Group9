d8fa4c233e674d0e7b9bbeee8fb4f0f1ae7c6d69
juliawegmayr <kontakt@juliawegmayr.at>
2022-12-12 22:15:14 +0100

    add navbar and stylesheet to contact.php

--- contact/contact.php
+++ contact/contact.php
@@ -47,15 +47,18 @@ if (isset($_POST['submit'])) {
 
    ?>
    
-   
-   
-   
    <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title></title>
+   <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/49748d0fd6.js" crossorigin="anonymous"></script>
+    <title>Contact Us</title>
    </head>
    <body>
+   <?php require_once "../components/navbar.php" ?>
 
         <p>Contact Us</p>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
