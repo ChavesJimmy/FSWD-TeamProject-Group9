9d525f3de839c44060fd2350ce3a3c2ac1a97f89
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-12-13 11:13:21 +0100

    update search list on user/index

--- user_panel/productslist.php
+++ user_panel/productslist.php
@@ -11,21 +11,35 @@ if(mysqli_num_rows($result)==0) {
     while($rows=mysqli_fetch_array($result, MYSQLI_ASSOC)){
         if(empty($search)){
             echo"";}
-            else{
-
-        echo "<div class='card col-4 m-auto' style='width: 18rem;'>
-        <img src='../pictures/".$rows['picture'] ."' class='card-img-top' alt='".$rows['name']."'>
-        <div class='card-body'>
-          <h5 class='card-title'>".$rows['name']."</h5>
-        </div>
-        <ul class='list-group list-group-flush'>
-          <br>(".$rows['price']." EUR - discount ".$rows['Discount']."%)</li>
-          <li class='list-group-item'>A third item</li>
-        </ul>
-        <div class='card-body'>
-          <a href='details.php?id=".$rows['id']."' class='card-link'>Details</a>
-        </div>
-      </div>";
+            else{if ($rows['Discount']>0){
+              echo "<div class='card col-4 m-auto' style='width: 18rem;'>
+              <img src='../pictures/".$rows['picture'] ."' class='card-img-top' alt='".$rows['name']."'>
+              <div class='card-body'>
+                <h5 class='card-title'>".$rows['name']."</h5>
+              </div>
+              <ul class='list-group list-group-flush'>New Price : 
+              ".$rows['price']-($rows['price']*$rows['Discount']/100)." EUR  
+              <br>(".$rows['price']." EUR - discount ".$rows['Discount']."%)</li>
+              </ul>
+              <div class='card-body'>
+                <a href='details.php?id=".$rows['id']."' class='card-link'>Details</a>
+              </div>
+            </div>";
+            }else{
+              echo "<div class='card col-4 m-auto' style='width: 18rem;'>
+              <img src='../pictures/".$rows['picture'] ."' class='card-img-top' alt='".$rows['name']."'>
+              <div class='card-body'>
+                <h5 class='card-title'>".$rows['name']."</h5>
+              </div>
+              <ul class='list-group list-group-flush'>
+                <br>Price : ".$rows['price']." EUR</li>
+              </ul>
+              <div class='card-body'>
+                <a href='details.php?id=".$rows['id']."' class='card-link'>Details</a>
+              </div>
+            </div>";
+      
+            }
       }
 
     
