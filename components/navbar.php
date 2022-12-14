0bd0e978e210414ef4a2977a2894e63263b75c4d
Katharina Pranjic <katharinapranjic@gmail.com>
2022-12-14 08:55:19 +0100

    update navbar

--- components/navbar.php
+++ components/navbar.php
@@ -2,7 +2,7 @@
         <div class="container-fluid">
             <a class="navbar-brand w-25" href="../index.php">
                 <div class="logo d-flex flex-column align-items-start">
-                    <img class="logo-img flex-fill w-75 " src="../img/logo.png">
+                    <img class="logo-img flex-fill w-75 " src="./img/logo.png">
                 </div>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,10 +17,10 @@
                         <a class="nav-link"  href="crud/login.php"><i class="fa-solid fa-magnifying-glass"></i></a>
                     </li> 
                     <li class="nav-item">
-                        <a class="nav-link"  href="../user_panel/shopping_cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
+                        <a class="nav-link"  href="./user_panel/shopping_cart.php"><i class="fa-solid fa-cart-shopping"></i></a>
                     </li>  
                     <li class="nav-item">
-                        <a class="nav-link"  href="../login.php"><i class="fa-solid fa-user"></i></a>
+                        <a class="nav-link"  href="./login.php"><i class="fa-solid fa-user"></i></a>
                     </li> 
                 </ul>
         </div>
@@ -34,8 +34,8 @@
     <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
     <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="../products/supplements.php">Supplements</a></li>
-      <li><a class="dropdown-item" href="../products/equipment.php">Equipment</a></li>
+      <li><a class="dropdown-item" href="./products/supplements.php">Supplements</a></li>
+      <li><a class="dropdown-item" href="./products/equipment.php">Equipment</a></li>
     </ul>
     </li>
 </ul>
